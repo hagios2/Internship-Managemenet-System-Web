a3a928ada9fb6ba99b488494ff35b3b5d9652dfb
hagios <hagioswilson@gmail.com>
2020-01-22 14:41:39 +0000

    trying to refactor script

--- resources/views/main_cordinator/other_application.blade.php
+++ resources/views/main_cordinator/other_application.blade.php
@@ -80,127 +80,96 @@
 @stop
 
 
-
 @section('js')
     <script> /* console.log('Hi!'); */
 
         $(document).ready(function(){
 
+            /* open the section with student proposed company on page load */
+
             proposed_application();
 
-            $('a#proposed_request').click(function(){
+            /* when click on btn for proposed company open section with proposed company */
 
-                $('div#main_studapp').show();
-               // $('div#open_letter').css({display: 'block'});
-                $('div#main_open_letter').hide();
+            $('a#proposed_request').click(function(){ proposed_application(); });
 
-                proposed_application();
-            });
+            /* when click on btn for request_open_letter, open section with open letter request  */
+
+            $('a#open_request').click(function(){open_letter_application()});
+
+
+            /* this function retrives data for proposed company application  */
 
             function proposed_application(){
 
-                $.ajax({    
+                $('div#main_studapp').show();
+            // $('div#open_letter').css({display: 'block'});
+                $('div#main_open_letter').hide();
+
+                /* get response data object */
+
+                var data = get_applications('/main-cordinator/proposed-applications');
+
+                /* setup table headers and no. of appliation  */
+
+                $('span.myspan1').html('<strong> No. of applications: ' + 8 + '</strong>');
+
+                $('div#studapp').html('<table class="table table-striped"><thead><th>Student Name</th><th>Proposed Company</th><th>Location</th><th>Region</th></thead><tbody class="app"></tbody></table>');
+
+                /* iterate tru data and display in the DOM */
+                $.map(data, function(data, u){
+
+                    $.each(data, function(i, application){
+
+                        $('tbody.app').append('<tr><td>' + application.student_name + '</td><td>'+ application.preferred_company_name + '</td><td>' + application.preferred_company_location + '</td><td>' + application.preferred_company_city + '</td></tr>');
+                    });
+                });   
                     
-                    method: 'GET',
-                    url: '/main-cordinator/proposed-applications',
+            }
+
+            /* make actual ajax request here and return data object */
+             function get_applications(var uri){
+
+                 return
+
+                $.ajax({    
+
+                    url: uri, 
                     dataType :'json'
                     
                 }).done(function(data){
-
-                    $('span.myspan1').html('<strong> No. of applications: ' + 8 + '</strong>');
-
-                    $('div#studapp').html('<table class="table table-striped"><thead><th>Student Name</th><th>Proposed Company</th><th>Location</th><th>Region</th></thead><tbody class="app"></tbody></table>');
-
-
-                    $.map(data, function(data, u){
-
-                       $.each(data, function(i, application){
-
-                            $('tbody.app').append('<tr><td>' + application.student_name + '</td><td>'+ application.preferred_company_name + '</td><td>' + application.preferred_company_location + '</td><td>' + application.preferred_company_city + '</td></tr>');
-                        });
-                    });                
-
-                    });
-                }
-
-                /* for open letter application */
-
-                $('a#open_request').click(function(){
-
-                    $('div#main_studapp').hide();
-                    $('div#main_open_letter').css({display: 'block'});
-                    $('div#main_open_letter').show();
-
-                    $.ajax({
-                        
-                        url: '/main-cordinator/requests-for-open-letter',
-                        dataType: 'json'
                     
-                    }).done(function(data){
-
-                        $('div#open_letter').html('<table class="table table-striped"><thead><th>Student Name</th><th>Phone</th><th>Program</th><th>Level</th></thead><tbody class="app1"></tbody></table>');
-
-                        $('span.myspan2').html('<strong> No. of applications: ' +8 + '</strong>');
-
-                        $.map(data, function(data, u){
-
-                            $.each(data, function(i, application){
-
-                                $('tbody.app1').append('<tr><td>' + application.student_name + '</td><td>'+ application.phone + '</td><td>' + application.program + '</td><td>' + application.level + '</td></tr>');
-                            });
-                        }); 
-
-                    });
+                    return  data;
+                
                 });
 
-            /*
-            $('#ki').click(function(){
-
-                $('div.me').toggle();
-            }); 
-            $('#dan1').keyup(function(e){
-
-                console.log(e.target.value);
-            });
-
-
-            $('#local').submit(function(e){
-               e.preventDefault();
-               alert('Event prevented') 
-
-                var name = $('#dan1').val();
-
-                var name1 = $('#dan2').val();
-
-                var name2 = $('#dan3').val();
-
-            var name3 = $('#dan4').val(); 
+                //return requestData;
                 
-                if(name == 'Emmanuel')
-                {
-                    $('.ti').css({display: 'block', color: 'blue'});
+            }
 
-                 $('.ti').html('<p>The first guy is called ' + name + '</p> <br><h4> Hello ' + name1 + '!</h4><div>' + name2 + ' is an amazing place  </div>'); 
-                }
-                
-            }); 
+            /* for open letter application */
 
-            $('#dan2').keyup(function(e){
+            function open_letter_application(){            
 
-                var code = e.which;
+                $('div#main_studapp').hide();
+                $('div#main_open_letter').css({display: 'block'});
+                $('div#main_open_letter').show();
 
-                if(code == 13)
-                {
-                    e.preventDefault();
+                var data = get_applications('/main-cordinator/requests-for-open-letter');
 
-                    $('#local').after('<h3>' + e.target.value + '</h3>');
-                }
-            });
+                $('div#open_letter').html('<table class="table table-striped"><thead><th>Student Name</th><th>Phone</th><th>Program</th><th>Level</th></thead><tbody class="app1"></tbody></table>');
 
-            
-            */
+                $('span.myspan2').html('<strong> No. of applications: ' +8 + '</strong>');
 
-        
+                $.map(data, function(data, u){
+
+                    $.each(data, function(i, application){
+
+                        $('tbody.app1').append('<tr><td>' + application.student_name + '</td><td>'+ application.phone + '</td><td>' + application.program + '</td><td>' + application.level + '</td></tr>');
+                    });
+                }); 
+
+            }
         }); 
        
      </script>)
