1b1b9fc1d75c71580ba31f356fa496ac5af671e4
hagios <hagioswilson@gmail.com>
2020-05-02 07:25:40 +0100

    hidding dates

--- app/ToggleApp.php
+++ app/ToggleApp.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ToggleApp extends Model
 {
     protected $guarded = ['id'];
+
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ];
+
 }
